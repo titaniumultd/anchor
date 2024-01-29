72b2703633d7d6e460b9fea457333588cc141957
SixxyPanda <deredevo@hotmail.com>
2024-01-28 17:13:34 -0800

    Add documentation for general architecture

--- src/common/interfaces/engine_interface.py
+++ src/common/interfaces/engine_interface.py
@@ -7,6 +7,19 @@ from src.common.interfaces.state_interface import ANStateControllerInterface
 class ANEngineInterface(ABC):
     """
     Interface declaration for the engine.
+
+    The engine should contain all internal business logic, separated into controllers.
+    
+    Public methods to engine and controller classes should be defined in an interface so that we don't
+    need to reference their respective class directly, therefore keeping dependencies clean.
+
+    Example:
+                                     Engine
+    |--------|        via         |--------------------------------------------------------|
+    |  View  |      ------>       |                           via                          |
+    |--------|  Engine Interface  |  Profile Controller  <------------>  State Controller  |
+                                  |                         Interface                      |
+                                  |--------------------------------------------------------|
     """
 
     @abstractmethod
