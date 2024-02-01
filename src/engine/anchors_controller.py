c759d5d38c61846e22932de096825e5b6fb62f77
titnium <titaniumultd@gmail.com>
2024-02-01 05:54:39 -0700

    remove profile dependancies

--- src/engine/anchors_controller.py
+++ src/engine/anchors_controller.py
@@ -6,7 +6,7 @@ from src.common.anchor import ANAnchor
 from src.common.config import MAX_ANCHORS
 from src.common.interfaces.engine_interface import ANEngineInterface
 from src.common.interfaces.anchors_interface import ANAnchorsControllerInterface
-from src.common.interfaces.profile_interface import ANProfileControllerInterface
+#from src.common.interfaces.profile_interface import ANProfileControllerInterface
 from src.common.interfaces.state_interface import ANStateControllerInterface
 
 
@@ -23,14 +23,14 @@ class ANAnchorsController(ANAnchorsControllerInterface, object):
     # Public Methods
 
     def load_anchors(self) -> list:
-        profile_controller = self._get_profile_controller()
+#        profile_controller = self._get_profile_controller()
 
-        if profile_controller.has_profiles():
-            current_profile = profile_controller.get_current_profile()
-            raw_anchors = self._get_profile_anchors(current_profile)
-            for anchor_dict in raw_anchors:
-                new_anchor = ANAnchor.from_dict(anchor_dict, self._get_root(), self._get_mouse(), self._get_notifier())
-                self._anchors.append(new_anchor)
+#        if profile_controller.has_profiles():
+#            current_profile = profile_controller.get_current_profile()
+#            raw_anchors = self._get_profile_anchors(current_profile)
+#            for anchor_dict in raw_anchors:
+#                new_anchor = ANAnchor.from_dict(anchor_dict, self._get_root(), self._get_mouse(), self._get_notifier())
+#                self._anchors.append(new_anchor)
 
         while len(self._anchors) < MAX_ANCHORS:
             self._drop_an_anchor()
@@ -39,12 +39,12 @@ class ANAnchorsController(ANAnchorsControllerInterface, object):
         return self._anchors
     
     def save_anchors(self) -> None:
-        profile_controller = self._get_profile_controller()
+#        profile_controller = self._get_profile_controller()
 
-        if profile_controller.has_profiles():
-            current_profile = profile_controller.get_current_profile()
-            profile_controller.update_profile(current_profile, [anchor.to_dict() for anchor in self._anchors])
-    
+#        if profile_controller.has_profiles():
+#            current_profile = profile_controller.get_current_profile()
+#            profile_controller.update_profile(current_profile, [anchor.to_dict() for anchor in self._anchors])
+        pass
     # Private Methods
 
     def _get_root(self):
@@ -59,19 +59,19 @@ class ANAnchorsController(ANAnchorsControllerInterface, object):
     def _get_state_controller(self) -> ANStateControllerInterface:
         return self._engine().get_state_controller()
 
-    def _get_profile_controller(self) -> ANProfileControllerInterface:
-        return self._engine().get_profile_controller()
+#    def _get_profile_controller(self) -> ANProfileControllerInterface:
+#        return self._engine().get_profile_controller()
     
-    def _get_profile_anchors(self, name: str) -> list:
-        profile_controller = self._get_profile_controller()
+    def _get_profile_anchors(self) -> list:
+#        profile_controller = self._get_profile_controller()
 
-        if profile_controller.has_profiles():
-            profiles = profile_controller.get_profiles()
+#        if profile_controller.has_profiles():
+#            profiles = profile_controller.get_profiles()
 
-            if name in profiles:
-                return profiles[name]
-            else:
-                logging.error(f"No such profile: {name}")
+#            if name in profiles:
+#                return profiles[name]
+#            else:
+#                logging.error(f"No such profile: {name}")
 
         return []
     
